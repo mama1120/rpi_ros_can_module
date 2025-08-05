4264e460880b1ebc48bd13ba8ae9b354f9d56852
Max Mai <mama1120@h-ka.de>
2025-08-05 20:03:12 +0200

    changed the absolute path of the cycloneDDS file to a relative path, to better accommodate the repo

--- start_docker.sh
+++ start_docker.sh
@@ -7,7 +7,7 @@ CONTAINER_NAME="ros2_moveit_container"
 xhost +local:docker
 
 # Run the container with the necessary settings for graphical interfaces and assign a name
-docker run -it --rm --network host -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp -e ROS_DOMAIN_ID=0 -e CYCLONEDDS_URI=///root/cyclonedds_config/CycloneDDS.xml -v /home/rpi5/cyclonedds_config:/root/cyclonedds_config --volume=/etc/localtime:/etc/localtime:ro --volume=/etc/timezone:/etc/timezone:ro --name $CONTAINER_NAME ros2-moveit:v1.2   #--device /dev/vcan0 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged
+docker run -it --rm --network host -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp -e ROS_DOMAIN_ID=0 -e CYCLONEDDS_URI=///root/cyclonedds_config/CycloneDDS.xml -v $(pwd)/cyclonedds_config:/root/cyclonedds_config --volume=/etc/localtime:/etc/localtime:ro --volume=/etc/timezone:/etc/timezone:ro --name $CONTAINER_NAME ros2-moveit:v1.2   #--device /dev/vcan0 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged
 
 # Function to enter the running container from another terminal
 docker exec -it $CONTAINER_NAME /bin/bash
